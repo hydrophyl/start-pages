d57fe120a2e340fd790d8bcc21ab8ce61e025d05
hydrophyl <forever_mind@live.com>
2020-11-08 12:44:51 +0100

    create pivot table

--- src/pages/start-page.jsx
+++ src/pages/start-page.jsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { Text } from "@fluentui/react";
+import { Text, Icon, Pivot, PivotItem, Label, Link } from "@fluentui/react";
 import "./start-page.scss";
 
 const StartPage = () => {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <Text variant="mega" block>
         Welcome to the internet!
+        <Icon iconName="Brightness" style={{ paddingTop: "0.5em" }} />
       </Text>
+
+      <Pivot aria-label="List of hypes">
+        <PivotItem headerText="Grandweg" itemCount={1}>
+          <Label>Grandweg Wohnanlage</Label>
+          <Link
+            className="link"
+            href="https://docs.google.com/spreadsheets/d/1yRncrPSWK4jXz4R6M8Y5_Wnp3e3WVjHi3q_Ksz8vzlY/edit#gid=759655960"
+          >
+            Waschmaschinen List
+          </Link>
+        </PivotItem>
+        <PivotItem headerText="HAW" itemCount={3}>
+          <Label>HAW Links</Label>
+          <Link className="link" href="https://haw-mailer.haw-hamburg.de/owa">
+            HAW-Mailer
+          </Link>
+          <Link
+            className="link"
+            href="https://myhaw.haw-hamburg.de/qisserver/pages/cs/sys/portal/hisinoneStartPage.faces?chco=y"
+          >
+            myHAW
+          </Link>
+          <Link className="link" href="https://emil.haw-hamburg.de/my/">
+            EMIL
+          </Link>
+        </PivotItem>
+      </Pivot>
       <div className="background"></div>
     </div>
   );
