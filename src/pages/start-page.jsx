b3bbf11f85040dd149b64787ed4fabc12ff2835f
hydrophyl <forever_mind@live.com>
2020-11-09 01:29:06 +0100

    add spotify and searchbox

--- src/pages/start-page.jsx
+++ src/pages/start-page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import {
   Text,
-  Icon,
   Pivot,
   PivotItem,
   Link,
   DefaultEffects,
   Stack,
+  Icon,
+  FontIcon,
+  SearchBox,
   getTheme,
 } from "@fluentui/react";
 import "./start-page.scss";
@@ -15,50 +18,63 @@ import Categories from "./categories.json";
 const theme = getTheme();
 
 const StartPage = () => {
+  const history = useHistory();
   return (
-    <div
-      id="start"
-      style={{ textAlign: "center", background: theme.palette.neutralDark }}
-    >
+    <div id="start" style={{ textAlign: "center" }}>
+      <div className="background" />
       <Text variant="mega" block className="title">
-        Welcome to the internet!
-        <Icon iconName="Brightness" style={{ paddingTop: "0.5em" }} />
+        Welcome to the abyss!
       </Text>
-
-      <Pivot
-        aria-label="List of hypes"
-        className="main-content"
+      <Stack
+        className="nav-board"
         style={{
           boxShadow: DefaultEffects.elevation8,
           background: theme.palette.neutralPrimary,
         }}
       >
-        {Object.values(Categories).map((category) => {
-          return (
-            <PivotItem
-              headerText={category.categoryname}
-              itemCount={category.links.length}
-            >
-              <Stack
-                horizontal
-                wrap
-                horizontalAlign="space-evenly"
-                style={{ marginTop: "0.8em" }}
+        <Pivot aria-label="List of hypes" className="pivot-board">
+          {Object.values(Categories).map((category) => {
+            return (
+              <PivotItem
+                headerText={category.categoryname}
+                itemCount={category.links.length}
               >
-                {Object.values(category.links).map((link) => {
-                  console.log(link.url);
-                  return (
-                    <Link className="link" href={link.url}>
-                      {link.name}
-                    </Link>
-                  );
-                })}
-              </Stack>
-            </PivotItem>
-          );
-        })}
-      </Pivot>
-      <div className="background"></div>
+                <Stack
+                  horizontal
+                  wrap
+                  horizontalAlign="space-evenly"
+                  style={{ marginTop: "0.8em", maxWidth: "600px" }}
+                >
+                  {Object.values(category.links).map((link) => {
+                    console.log(link.url);
+                    return (
+                      <Link className="link" href={link.url}>
+                        {link.name}
+                      </Link>
+                    );
+                  })}
+                </Stack>
+              </PivotItem>
+            );
+          })}
+        </Pivot>
+        <SearchBox
+          onSearch={(e) => history.push("https://duckduckgo.com/?q=" + { e })}
+          underlined={true}
+          style={{ background: theme.palette.neutralPrimary, border: "none" }}
+          placeholder="d/g/y/di/"
+        />
+      </Stack>
+      <iframe
+        title="spotify"
+        src="https://open.spotify.com/embed/playlist/55z05L7uSns7aPVkeHvRkO"
+        width="600"
+        height="280"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+        style={{ margin: "auto" }}
+      ></iframe>
     </div>
   );
 };
